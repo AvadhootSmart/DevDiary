8661ed36aba324772bdfecbda984129eeb1df932
AvadhootSmart <1234avadhootsmart@gmail.com>
2024-06-20 01:40:06 +0530

    removed line animations

--- blog-app/src/components/Title.jsx
+++ blog-app/src/components/Title.jsx
@@ -9,35 +9,13 @@ function Title() {
       opacity: 0,
       y: 100,
     });
-    gsap.fromTo(
-      ".Left",
-      {
-        duration: 0.8,
-        x: -200,
-      },
-      { x: 0 }
-    );
-    gsap.fromTo(
-      ".Right",
-      {
-        x: 200,
-      },
-      {
-        x: 0,
-        duration: 0.8,
-      }
-    );
   });
   return (
     <>
       <div className="flex justify-around text-[#b8b4b0] w-full mt-[20vh] items-center">
-        <div className="Left">
-          <Line />
-        </div>
+        <Line />
         <h2 className="Heading text-4xl font-[Alegreya] uppercase">Av Blogs</h2>
-        <div className="Right">
-          <Line />
-        </div>
+        <Line />
       </div>
     </>
   );
